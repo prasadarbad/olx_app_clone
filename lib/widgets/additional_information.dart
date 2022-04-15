cf180db415f26ba11b67011829da662291a2038a
Prasad Arbad <prasadarbad9@gmail.com>
2022-04-15 09:15:09 +0530

    additional information widget added

---
+++ lib/widgets/additional_information.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class AdditionalInformation extends StatefulWidget {
+  const AdditionalInformation({Key? key}) : super(key: key);
+
+  @override
+  State<AdditionalInformation> createState() => _AdditionalInformationState();
+}
+
+class _AdditionalInformationState extends State<AdditionalInformation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 300,
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Title(
+                color: Colors.black,
+                child: const Text(
+                  'Additional INFORMATION',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(fontWeight: FontWeight.w500, fontSize: 23),
+                ),
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              const Text(
+                'Description',
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey),
+              ),
+              TextFormField(
+                maxLength: 500,
+                maxLines: 5,
+                decoration: InputDecoration(
+                  contentPadding: const EdgeInsets.fromLTRB(7, 7, 7, 0),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    borderSide:
+                        const BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    borderSide:
+                        const BorderSide(color: Colors.grey, width: 1.0),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
